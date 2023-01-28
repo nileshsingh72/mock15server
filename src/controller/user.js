3e6c0a2957bec39e43311cc0923738bffa7c1ceb
nileshsingh72 <rnileshchauhan89@gmail.com>
2023-01-28 13:00:31 +0530

    server completed

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,58 +1,60 @@
-const User = require("../model/user")
-const createUser = async(req, res)=>{
-   const {email,password ,name } = req.body;
-    try{
-      let find = await User.find({email})  
-      if(find.length > 0){
+const User = require("../model/user");
+const createUser = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    let find = await User.find({ email });
+    if (find.length > 0) {
+      res.send({
+        status: false,
+        message: "Email is already exists !",
+      });
+    } else {
+      let user = await User.create({ name, email, password });
+      res.send({
+        status: true,
+        message: "Your Account is created successfully",
+        user,
+      });
+    }
+  } catch (e) {
+    res.send({
+      status: false,
+      message: e.message,
+    });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let find = await User.find({ email });
+    if (find.length <= 0) {
+      res.send({
+        status: false,
+        message: "Email or password is incorrect!",
+      });
+    } else {
+      if (find[0].password == password) {
+        const token = find[0]._id;
         res.send({
-            status:false,
-            message:'Email is already exists !'
-        })
+          status: true,
+          message: "Login Successfully ! ",
+          token: token,
+          user: find[0],
+        });
+      } else {
+        res.send({
+          status: false,
+          message: "Incorrect password ! ",
+        });
       }
-      else{
-             
-                let user = await User.create({name,email, password});
-                res.send({
-                    status:true,
-                    message:'Your Account is created successfully',
-                    user
-                })
-         }
     }
-    catch(e){
-        res.send({
-            status:false ,
-            message:e.message
-        })
-    }
-}
+  } catch (e) {
+    res.send({
+      status: false,
+      message: e.message,
+    });
+  }
+};
 
-
-
-
-const loginUser = async(req, res)=>{
-    const {email, password} = req.body;
-     try{
-       let find =await User.find({email , password})  
-       if(find.length <= 0){
-         res.send({
-             status:false,
-             message:'Email or password is incorrect!'
-         })
-       }
-       else{
-              const token = find[0]._id
-              res.send({ status : true , message: "Login Successfully ! ", token: token , user:find[0] });
-          }
-    }
-     catch(e){
-         res.send({
-             status:false ,
-             message:e.message
-         })
-     }
- }
-
-
-
-module.exports = {createUser , loginUser}
+module.exports = { createUser, loginUser };
