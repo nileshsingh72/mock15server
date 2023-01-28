f04ee4a2ed055fa025413da4bcfc7ff90722852c
nileshsingh72 <rnileshchauhan89@gmail.com>
2023-01-28 14:10:00 +0530

    server completed

--- src/controller/bmi.js
+++ src/controller/bmi.js
@@ -1,42 +1,39 @@
 const Mass = require("../model/bmi");
 
-const createBMI = async(req , res)=>{
-     let userID = req.params.id;
-     const {h , w} = req.body;
-     try{
-        let hi  = h*0.304;
-        let calBMI = w / hi;
-        let f = calBMI.toFixed(2);
-        let cal = await Mass.create({userID , BMI :f})
-        res.send({
-            status:true , 
-            BMI:f
-        })
-     }
-     catch(e){
-        res.send({
-            status:false , 
-            message:e.message
-        })
-     }
-}
+const createBMI = async (req, res) => {
+  let userID = req.params.id;
+  const { height, weight } = req.body;
+  try {
+    let hi = Number(height) * 0.304;
+    let calBMI = Number(weight) / hi;
+    let f = calBMI.toFixed(2);
+    let cal = await Mass.create({ userID, BMI: f });
+    res.send({
+      status: true,
+      BMI: f,
+    });
+  } catch (e) {
+    res.send({
+      status: false,
+      message: e.message,
+    });
+  }
+};
 
+const getHistory = async (req, res) => {
+  let userID = req.params.id;
+  try {
+    let find = await Mass.find({ userID });
+    res.send({
+      status: true,
+      data: find,
+    });
+  } catch (e) {
+    res.send({
+      status: false,
+      message: e.message,
+    });
+  }
+};
 
-const getHistory = async(req, res)=>{
-     let userID = req.params.id;
-     try{
-      let find = await Mass.find({userID});
-        res.send({
-            status:true , 
-            data:find
-        })
-     }
-     catch(e){
-        res.send({
-           status:false , 
-           message : e.message
-        })
-     }
-}
-
-module.exports = {getHistory , createBMI}
+module.exports = { getHistory, createBMI };
